6bf0367d782048374004225dc20f1e8c05400f76
Ivan-Kr <butunska5@gmail.com>
2021-11-29 22:15:18 +0200

    try to made snake game

--- Wanichuka/Program.cs
+++ Wanichuka/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Wanichuka
 {
     class Program 
     {
-        static void Main(string[] args) 
+        static void Main() 
         {
-            float a=0.1245f;
-            Console.WriteLine(a);
+            int x = 16, y = 8;
+            int[,] map = new int[x, y];
+
+            ///////////////////////////
+            
+            List<int> zaglushka = new List<int>();
+            zaglushka.Add(x / 2);
+            zaglushka.Add(y / 2);
+
+            List<List<int>> snake=new List<List<int>>();
+
+            snake.Add(zaglushka);
+
+            ///////////////////////////
+
+            while (true)
+            {
+                
+            }
+
         }
     }
 }
