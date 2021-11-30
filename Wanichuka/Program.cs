80c656ea4b48171f6f950969c2946756d9dbdd9b
Ivan-Kr <butunska5@gmail.com>
2021-11-30 21:38:47 +0200

    made simple logic but forget my map

--- Wanichuka/Program.cs
+++ Wanichuka/Program.cs
@@ -4,23 +4,29 @@ using System.Collections.Generic;
 
 namespace Wanichuka
 {
+
+    class Map
+    {
+
+    }
+
     class Tail
     {
         private int _x;
         private int _y;
         
-        public void slide(int x,int y)
+        public void Slide(int x,int y)
         {
             _x = x;
             _y = y;
         }
 
-        public int x()
+        public int X()
         {
             return _x;
         }
 
-        public int y()
+        public int Y()
         {
             return _y;
         }
@@ -30,18 +36,33 @@ namespace Wanichuka
     {
         private List<Tail> tails = new List<Tail>(1);
 
+        public enum Direction
+        {
+            up,
+            right,
+            down,
+            left
+        } Direction dir;
+
         public Snake(int start_x,int start_y)
         {
-
+            tails.Add(new Tail());
+            dir = Direction.up;
+            tails[0].Slide(start_x, start_y);
         }
 
-        public void new_tail()
+        public void NewTail()
         {
             tails.Add(new Tail());
-            tails.ElementAt(tails.Count - 1).slide(tails.ElementAt(tails.Count-2).x(), tails.ElementAt(tails.Count - 2).y());
+
+            int preview = tails.Count - 1;
+
+            tails[preview].Slide(tails[preview-1].X(), tails[preview - 1].Y());
         }
 
     }
+
+    
 
     class Program 
     {
