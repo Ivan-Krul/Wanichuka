45819534515c102fbd915c7888d8398cbf842251
Ivan-Kr <butunska5@gmail.com>
2021-11-30 21:27:23 +0200

    made class tail and snake

--- Wanichuka/Program.cs
+++ Wanichuka/Program.cs
@@ -1,31 +1,53 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Wanichuka
 {
+    class Tail
+    {
+        private int _x;
+        private int _y;
+        
+        public void slide(int x,int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int x()
+        {
+            return _x;
+        }
+
+        public int y()
+        {
+            return _y;
+        }
+    }
+
+    class Snake
+    {
+        private List<Tail> tails = new List<Tail>(1);
+
+        public Snake(int start_x,int start_y)
+        {
+
+        }
+
+        public void new_tail()
+        {
+            tails.Add(new Tail());
+            tails.ElementAt(tails.Count - 1).slide(tails.ElementAt(tails.Count-2).x(), tails.ElementAt(tails.Count - 2).y());
+        }
+
+    }
+
     class Program 
     {
         static void Main() 
         {
-            int x = 16, y = 8;
-            int[,] map = new int[x, y];
-
-            ///////////////////////////
             
-            List<int> zaglushka = new List<int>();
-            zaglushka.Add(x / 2);
-            zaglushka.Add(y / 2);
-
-            List<List<int>> snake=new List<List<int>>();
-
-            snake.Add(zaglushka);
-
-            ///////////////////////////
-
-            while (true)
-            {
-                
-            }
 
         }
     }
