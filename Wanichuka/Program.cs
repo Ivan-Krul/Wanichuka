e06990bd805f536fa991d1b2e1b17c0fc1c1547a
Ivan-Kr <butunska5@gmail.com>
2021-12-01 19:40:33 +0200

    made class map

--- Wanichuka/Program.cs
+++ Wanichuka/Program.cs
@@ -7,33 +7,70 @@ namespace Wanichuka
 
     class Map
     {
-
-    }
-
-    class Tail
-    {
+        private char[,] _plate;
         private int _x;
         private int _y;
-        
-        public void Slide(int x,int y)
+
+        public Map(int x,int y)
         {
             _x = x;
             _y = y;
+
+            _plate=new char[_x,_y];
         }
 
-        public int X()
+        public void Insert(int x,int y, char ch)
         {
-            return _x;
+            if (-1 < x && x < _x && -1 < y && y < _y) _plate[x, y] = ch;
         }
 
-        public int Y()
+        public char PullOut(int x,int y)
         {
-            return _y;
+            if (-1 < x && x < _x && -1 < y && y < _y) return _plate[x, y];
+            else return '\0';
         }
+
+        public void WriteOut()
+        {
+            string stroke="";
+            for (int i = 0; i < _y; i++)
+            {
+                for (int j = 0; j < _x; j++)
+                {
+                    stroke += _plate[j, i];
+                }
+            }
+            Console.WriteLine(stroke);
+
+        }
+
     }
 
+    
     class Snake
     {
+        private class Tail
+        {
+            private int _x;
+            private int _y;
+
+            public void Slide(int x, int y)
+            {
+                _x = x;
+                _y = y;
+            }
+
+            public int X()
+            {
+                return _x;
+            }
+
+            public int Y()
+            {
+                return _y;
+            }
+        }
+
         private List<Tail> tails = new List<Tail>(1);
 
         public enum Direction
@@ -42,7 +79,8 @@ namespace Wanichuka
             right,
             down,
             left
-        } Direction dir;
+        } 
+        private Direction dir;
 
         public Snake(int start_x,int start_y)
         {
@@ -68,7 +106,6 @@ namespace Wanichuka
     {
         static void Main() 
         {
-            
 
         }
     }
